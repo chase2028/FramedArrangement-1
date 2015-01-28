34113d328f9fc41c7446ce350b7478e924002b30
arbmos <oscarecho@fastmail.fm>
2015-01-27 23:03:39 -0800

    Add the views to the view controller

--- FramedArrangement/ViewController.m
+++ FramedArrangement/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) UIView *redView;
+@property (strong, nonatomic) UIView *blueView;
+@property (strong, nonatomic) UIView *greenView;
+@property (strong, nonatomic) UIView *yellowView;
+
 @end
 
 @implementation ViewController
@@ -17,6 +22,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // Add redView
+    self.redView = [UIView new];
+    self.redView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.redView];
+    
+    // Add blueView
+    self.blueView = [UIView new];
+    self.blueView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:self.blueView];
+    
+    // Add greenView
+    self.greenView = [UIView new];
+    self.greenView.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:self.greenView];
+    
+    // Add yellowView
+    self.yellowView = [UIView new];
+    self.yellowView.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:self.yellowView];
 }
 
 - (void)didReceiveMemoryWarning {
