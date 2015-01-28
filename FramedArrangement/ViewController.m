c1d8db37aa98aff25ce14389198ff28ec05930d6
arbmos <oscarecho@fastmail.fm>
2015-01-28 08:56:37 -0800

    Add layoutSquares method

--- FramedArrangement/ViewController.m
+++ FramedArrangement/ViewController.m
@@ -49,4 +49,32 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (int)layoutSquares {
+    CGRect frame = [UIScreen mainScreen].bounds;
+    CGRect part1;
+    CGRect part2;
+    CGRect frame1;
+    CGRect frame2;
+    CGRect frame3;
+    CGRect frame4;
+    CGRectDivide(frame, &part1, &part2, CGRectGetMaxX(frame), CGRectMaxYEdge);
+    CGRectDivide(part1, &frame1, &frame2, CGRectGetMaxY(part1), CGRectMaxXEdge);
+    CGRectDivide(part2, &frame3, &frame4, CGRectGetMaxY(part2), CGRectMaxXEdge);
+    
+    _redView = [[UIView alloc] initWithFrame:frame1];
+    _blueView = [[UIView alloc] initWithFrame:frame2];
+    _greenView = [[UIView alloc] initWithFrame:frame3];
+    _yellowView = [[UIView alloc] initWithFrame:frame4];
+    
+    int width = frame1.size.width;
+    int height = frame1.size.height;
+    int x = frame2.origin.x;
+    int y = frame3.origin.y;
+    
+    return width;
+    return height;
+    return x;
+    return y;
+}
+
 @end
